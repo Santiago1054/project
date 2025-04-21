29b4de9d101ecae892ecbddbe50e22e08910a633
Santiago Ramirez Castellanos <santiago5.ramirez@ucp.edu.co>
2025-04-21 13:41:50 -0500

    April 21th 2025 1:41PM

--- src/components/FirtstApi.jsx
+++ src/components/FirtstApi.jsx
@@ -4,28 +4,15 @@ import { Movies } from './Movies'
 import { useMovies } from '../hooks/useMovies'
 import { useEffect } from 'react'
 
-
-
-export function FirtstApi() {
-    const { movies: mappedMovies } = useMovies()
+function useSearch() {
     const [search, setSearch] = useState('')
     const [inputError, setInputError] = useState(null)
-    //const inputRef = useRef()
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log({ search })
-        /* const fields = Object.fromEntries(new window.FormData(event.target)) 
-        // Esta forma de obtener los datos es mas limpia y no necesita el useRef
-        //Usarlo cuando se tenga un formulario con muchos inputs
-        const data = new window.FormData(event.target)
-        const search = data.get('search')
-        console.log(search)*/
-        // TO DO : Validar el input 
-    }
-    const handleChange = (event) => {
-        setSearch(event.target.value)
-    }
+    const isFirstInput = useRef(true)
     useEffect(() => {
+        if (isFirstInput.current) {
+            isFirstInput.current = search == ''
+            return
+        }
         if (search == '') {
             setInputError('El campo de busqueda no puede estar vacio')
             return
@@ -43,19 +30,41 @@ export function FirtstApi() {
         setInputError(null)
 
     }), [search]
+    return { search, setSearch, inputError }
+}
+
+export function FirtstApi() {
+    const { movies: mappedMovies } = useMovies()
+    const { search, setSearch, inputError } = useSearch()
+    //const inputRef = useRef()
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log({ search })
+        /* const fields = Object.fromEntries(new window.FormData(event.target)) 
+        // Esta forma de obtener los datos es mas limpia y no necesita el useRef
+        //Usarlo cuando se tenga un formulario con muchos inputs
+        const data = new window.FormData(event.target)
+        const search = data.get('search')
+        console.log(search)*/
+        // TO DO : Validar el input 
+    }
+    const handleChange = (event) => {
+        setSearch(event.target.value)
+    }
+
     return (
         <>
             <div className='page'>
                 <header>
                     <h1 className='text-title'>Buscador de peliculas</h1>
                     <form className='form' onSubmit={handleSubmit}>
-                        <input onChange={handleChange} name='search' placeholder='Avengers, Star Wars, The Matrix ...' />
+                        <input  onChange={handleChange} name='search' placeholder='Avengers, Star Wars, The Matrix ...' />
                         <button type='submit'>Search</button>
                     </form>
                     {inputError && <p className='error'>{inputError}</p>}
                 </header>
 
-                <main className='text'>
+                <main >
                     <Movies movies={mappedMovies} />
                 </main>
             </div>
